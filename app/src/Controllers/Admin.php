67ef3ccb8898be9ef122f0d13389f50f4fe69fdc
Vesko Kenashkov <vesko@azonmedia.com>
2020-01-17 16:25:55 +0200

    wip

---
+++ app/src/Controllers/Admin.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace GuzabaPlatform\RequestCaching\Controllers;
+
+use Guzaba2\Http\Method;
+use GuzabaPlatform\Platform\Application\BaseController;
+use Psr\Http\Message\ResponseInterface;
+
+class Admin extends BaseController
+{
+    protected const CONFIG_DEFAULTS = [
+        'routes'        => [
+            '/admin/request-caching'    => [
+                Method::HTTP_GET              => [self::class, 'main']
+            ],
+        ],
+    ];
+
+    protected const CONFIG_RUNTIME = [];
+
+    public function main() : ResponseInterface
+    {
+
+    }
+}
